a854cdc26c86a883266a66a778ff3fbfb5cbe672
fabriciocotag <shutdownfabricio@gmail.com>
2023-05-20 10:25:16 -0300

    Exercício 36

--- scripts/test1.py
+++ scripts/test1.py
@@ -1,31 +1,7 @@
+from pathlib import Path
+import json
 import logging
 import requests
-
-logging.basicConfig()
-logger = logging.getLogger("Interagi Listagem")
-logger.setLevel(logging.INFO)
-
-BASE_URL="http://localhost:8080/Plone/++api++"
-
-LOGIN = {
-    "USUARIO": "admin",
-    "SENHA": "admin",
-}
-
-session = get_logged_session(LOGIN)
-
-response = session.get(BASE_URL)
-data = response.json()
-titulo = data["title"]
-logger.info(f"O título do portal é {titulo}")
-
-search_url = f"{BASE_URL}/@search?sort_on=path"
-response = session.get(BASE_URL)
-data = response.json()
-total_conteudo = data["items_total"]
-logger.info(f"O portal conta com {total_conteudo} itens de conteúdo")
-
-
 
 # Funções
 
@@ -47,3 +23,36 @@ def get_logged_session(LOGIN):
         {"Authorization": f"Bearer {token}"}
     )
     return session
+
+# Código
+
+logging.basicConfig()
+logger = logging.getLogger("Interagi Listagem")
+logger.setLevel(logging.INFO)
+
+PASTA_ATUAL = Path(__file__).parent.resolve()
+PASTA_DADOS = PASTA_ATUAL / "data"
+
+if not PASTA_DADOS.exists():
+    PASTA_DADOS.mkdir(parents=True, exist_ok=True)
+    logger.info(f"Criada a pasta {PASTA_DADOS}")
+
+BASE_URL="http://localhost:8080/Plone/++api++"
+
+LOGIN = {
+    "USUARIO": "admin",
+    "SENHA": "admin",
+}
+
+session = get_logged_session(LOGIN)
+
+response = session.get(BASE_URL)
+data = response.json()
+titulo = data["title"]
+logger.info(f"O título do portal é {titulo}")
+
+search_url = f"{BASE_URL}/@search?sort_on=path"
+response = session.get(BASE_URL)
+data = response.json()
+total_conteudo = data["items_total"]
+logger.info(f"O portal conta com {total_conteudo} itens de conteúdo")
